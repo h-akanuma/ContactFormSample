be70546a51a03e8860c9ca0e88fe7a33204c2a60
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-22 20:07:27 +0900

    List 8.6 p.199

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -6,6 +6,20 @@ class Controller_Form extends Controller_Template {
 		$this->template->title = 'コンタクトフォーム';
 		$this->template->content = View::forge('form/index');
 	}
+	
+	public function action_confirm() {
+		$val = $this->get_validation();
+		
+		if ($val->run()) {
+			$data['input'] = $val->validated();
+			$this->template->title = 'コンタクトフォーム：確認';
+			$this->template->content = View::forge('form/confirm', $data);
+		} else {
+			$this->template->title = 'コンタクトフォーム：エラー';
+			$this->template->content = View::forge('form/index');
+			$this->template->content->set_safe('html_error', $val->show_errors());
+		}
+	}
 
 	public function get_validation() {
 		$val = Validation::forge();
