f4d8d220c24b3217c3e22b9165991a6b2f2f9a53
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-22 19:54:35 +0900

    List 8.5 p.198

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -7,4 +7,24 @@ class Controller_Form extends Controller_Template {
 		$this->template->content = View::forge('form/index');
 	}
 
+	public function get_validation() {
+		$val = Validation::forge();
+		
+		$val->add('name', '名前')
+			->add_rule('trim')
+			->add_rule('required')
+			->add_rule('max_length', 50);
+		
+		$val->add('email', 'メールアドレス')
+			->add_rule('trim')
+			->add_rule('required')
+			->add_rule('max_length', 100)
+			->add_rule('valid_email');
+		
+		$val->add('comment', 'コメント')
+			->add_rule('required')
+			->add_rule('max_length', 400);
+		
+		return $val;
+	}
 }
