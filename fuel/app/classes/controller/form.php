12b696c433052db01ae896d0a7865995a5f9bd9d
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-22 21:53:02 +0900

    List 8.11 p.210

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -20,6 +20,43 @@ class Controller_Form extends Controller_Template {
 			$this->template->content->set_safe('html_error', $val->show_errors());
 		}
 	}
+	
+	public function action_send() {
+		// CSRF対策
+		if (!Security::check_token()) {
+			return 'ページ遷移が正しくありません。';
+		}
+		
+		$val = $this->get_validation();
+		
+		if (!$val->run()) {
+			$this->template->title = 'コンタクトフォーム：エラー';
+			$this->template->content = View::forge('form/index');
+			$this->template->content->set_safe('html_error', $val->show_errors());
+			return;
+		}
+		
+		$post = $val->validated();
+		$data = $this->build_mail($post);
+		
+		// メールの送信
+		try {
+			$this->sendmail($data);
+			$this->template->title = 'コンタクトフォーム：送信完了';
+			$this->template->content = View::forge('form/send');
+			return;
+		} catch (EmailValidationFailedException $e) {
+			Log::error('メール検証エラー：' . $e->getMessage(), __METHOD__);
+			$html_error = '<p>メールアドレスに誤りがあります。</p>';
+		} catch (EmailSendingFailedException $e) {
+			Log::error('メール送信エラー：' . $e->getMessage(), __METHOD__);
+			$html_error = '<p>メールを送信できませんでした。</p>';
+		}
+		
+		$this->template->title = 'コンタクトフォーム：送信エラー';
+		$this->template->content = View::forge('form/index');
+		$this->template->content->set_safe('html_error', $html_error);
+	}
 
 	public function get_validation() {
 		$val = Validation::forge();
@@ -40,5 +77,44 @@ class Controller_Form extends Controller_Template {
 			->add_rule('max_length', 400);
 		
 		return $val;
+	}
+	
+	// メールの作成
+	public function build_mail($post) {
+		$data['from'] = $post['email'];
+		$data['from_name'] = $post['name'];
+		$data['to'] = 'info@example.jp';
+		$data['to_name'] = '管理者';
+		$data['subject'] = 'コンタクトフォーム';
+		
+		$ip = Input::ip();
+		$agent = Input::user_agent();
+		
+		$data['body'] = <<< END
+-------------------------------------------------------------------
+名前：${post['name']}
+メールアドレス：${post['email']}
+IPアドレス：$ip
+ブラウザ：$agent
+-------------------------------------------------------------------
+コメント：
+{$post['comment']}
+-------------------------------------------------------------------
+END;
+
+		return $data;
+	}
+	
+	// メールの送信
+	public function sendmail($data) {
+		Package::load('email');
+		
+		$email = Email::forge();
+		$email->from($data['from'], $data['from_name']);
+		$email->to($data['to'], $data['to_name']);
+		$email->subject($data['subject']);
+		$email->body($data['body']);
+		
+		$email->send();
 	}
 }
