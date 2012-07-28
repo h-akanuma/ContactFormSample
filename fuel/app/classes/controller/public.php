30e764780bc02ba22948a0268fc31d34387a8ad7
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-28 14:49:33 +0900

    List 9.13 p.228

---
+++ fuel/app/classes/controller/public.php
@@ -0,0 +1,9 @@
+<?php
+
+class Controller_Public extends Controller_Template {
+	
+	public function before() {
+		parent::before();
+		$this->response->set_header('X-FRAME-OPTIONS', 'SAMEORIGIN');
+	}
+}
