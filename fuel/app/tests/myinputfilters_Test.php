9cf70db4ca7cc9779ed4ba3bb9e13f1e1f402583
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-28 13:24:34 +0900

    List 9.7 p.223

--- fuel/app/tests/myinputfilters_Test.php
+++ fuel/app/tests/myinputfilters_Test.php
@@ -21,4 +21,42 @@ class Test_MyInputFilters extends PHPUnit_Framework_TestCase {
 		
 		$this->assertEquals($expected, $test);
 	}
+	
+	/**
+	 * @dataProvider newline_provider
+	 */
+	public function test_check_control_改行とタブを含む文字列($data) {
+		$input = $data;
+		$test = MyInputFilters::check_control($input);
+		$expected = $input;
+		
+		$this->assertEquals($expected, $test);
+	}
+	
+	public function newline_provider() {
+		return array(
+			array("改行を含む\n文字列です。"),
+			array("改行を含む\r文字列です。"),
+		    array("改行を含む\n\r文字列です。"),
+			array("タブを含む\t文字列です"),
+			array("改行と\rタブを含む\t文字列です。"),
+		);
+	}
+	
+	/**
+	 * @dataProvider control_code_provider
+	 */
+	public function test_check_control_制御文字を含む文字列($data) {
+		$this->setExpectedException('HttpInvalidInputException', 'Invalid input data');
+		
+		$input = $data;
+		$test = MyInputFilters::check_control($input);
+	}
+	
+	public function control_code_provider() {
+		return array(
+			array("NULL文字を含む文字列です。\0"),
+			array("NULL文字と改行コードを含む文字列です。\0\n"),
+		);
+	}
 }
