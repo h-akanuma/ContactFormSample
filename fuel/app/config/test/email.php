d0627766ecf97ce090fec61e59fdf0d399dd25bc
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-28 22:24:45 +0900

    List 9.30 p.244

---
+++ fuel/app/config/test/email.php
@@ -0,0 +1,6 @@
+<?php
+
+// PHP の mail() 関数をオーバーライド
+require_once APPPATH . 'tests/mock_mail.php';
+
+return array();
---
+++ fuel/app/tests/mock_mail.php
@@ -0,0 +1,8 @@
+<?php
+
+// PHP の mail() 関数をオーバーライド
+namespace Email;
+
+function mail($to, $subject, $message, $additional_headers, $additional_parameters) {
+	return true;
+}
