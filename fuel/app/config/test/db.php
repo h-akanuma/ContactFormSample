821567519961880614835accce45207f8b6867e1
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-29 22:49:13 +0900

    List 10.2 p.262

---
+++ fuel/app/config/test/db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The test database settings.
+ */
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'hostname'   => 'localhost',
+			'port'       => '3306',
+			'database'   => 'contact_db_test',
+			'username'   => 'username',
+			'password'   => 'password',
+		),
+	),
+);
