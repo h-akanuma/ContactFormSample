c3bd647a9710f07676d561455dac8418f4e0decc
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-31 22:11:20 +0900

    Test_Model_Form クラス (fuel/app/tests/model/form_Test.php) の追加メソッド  p.274

--- fuel/app/tests/model/form_Test.php
+++ fuel/app/tests/model/form_Test.php
@@ -23,4 +23,26 @@ class Test_Model_Form extends DbTestCase {
 			}
 		}
 	}
+	
+	public function test_save_insert() {
+		$data = array(
+			'name' => '藤原義孝',
+			'email' => 'yoshitaka@example.jp',
+			'comment' => '君がため おしからざりし 命さえ 長くもがなと 思ひけるかな',
+			'ip_address' => '10.11.12.13',
+			'user_agent' => 'Mozilla/2.02 (Macintosh; I; PPC)',
+		);
+		
+		$form = Model_Form::forge()->set($data);
+		
+		// 新規データをデータベースに挿入
+		list($id, $rows) = $form->save();
+		
+		// 挿入されたデータをデータベースから検索
+		$form = Model_Form::find_by_pk($id);
+		
+		foreach($data as $field => $value) {
+			$this->assertEquals($value, $form[$field]);
+		}
+	}
 }
